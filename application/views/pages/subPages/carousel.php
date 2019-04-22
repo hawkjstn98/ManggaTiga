d04e7d6d80f25b4678af580fb8623ffdb19aa8fb
hawkjstn98 <hawk.jstn98@gmail.com>
2019-04-22 12:40:14 +0700

    update image path

--- application/views/pages/subPages/carousel.php
+++ application/views/pages/subPages/carousel.php
@@ -1,13 +1,13 @@
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img class="d-block w-100" src="http://localhost/manggatiga/assets/carousel/1.png" alt="First slide" width="600" height="400">
+            <img class="d-block w-100" src="<?php echo base_url('/assets/carousel/1.png')?>" alt="First slide" width="600" height="400">
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100" src="http://localhost/manggatiga/assets/carousel/2.png" alt="Second slide" width="600" height="400">
+            <img class="d-block w-100" src="<?php echo base_url('assets/carousel/2.png') ?>" alt="Second slide" width="600" height="400">
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100" src="http://localhost/manggatiga/assets/carousel/3.png" alt="Third slide" width="600" height="400">
+            <img class="d-block w-100" src="<?php echo base_url('assets/carousel/3.png') ?>" alt="Third slide" width="600" height="400">
         </div>
     </div>
         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
