8b1d8d200e97e724323651d5fd84dc84c7d1ebb2
frapslover63 <39822385+frapslover63@users.noreply.github.com>
2019-04-23 21:42:02 +0700

    Database Table

--- projectpemweb.sql
+++ projectpemweb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 23, 2019 at 07:34 AM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.11
+-- Host: localhost:3306
+-- Generation Time: Apr 23, 2019 at 02:41 PM
+-- Server version: 5.7.19
+-- PHP Version: 7.1.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,6 +25,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `barangId` int(11) NOT NULL,
+  `barangNama` varchar(200) NOT NULL,
+  `gambarNama` text NOT NULL,
+  `details` text NOT NULL,
+  `categoryId` int(11) NOT NULL,
+  `brandId` int(11) NOT NULL,
+  `hargaJual` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `aksesBarang` int(11) NOT NULL,
+  `promoId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brand`
+--
+
+CREATE TABLE `brand` (
+  `brandId` int(11) NOT NULL,
+  `brandNama` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `carousel_promo`
 --
 
@@ -34,18 +64,114 @@ CREATE TABLE `carousel_promo` (
   `path` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `carousel_promo`
+-- Table structure for table `category`
 --
 
-INSERT INTO `carousel_promo` (`id`, `description`, `path`) VALUES
-(1, 'Promo 1', '/assets/carousel/1.png'),
-(2, 'Promo 2', '/assets/carousel/2.png'),
-(3, 'Promo 3', '/assets/carousel/3.png');
+CREATE TABLE `category` (
+  `categoryId` int(11) NOT NULL,
+  `categoryNama` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gambar`
+--
+
+CREATE TABLE `gambar` (
+  `gambarId` int(11) NOT NULL,
+  `gambarPath` text NOT NULL,
+  `barangId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `promo`
+--
+
+CREATE TABLE `promo` (
+  `promoId` int(11) NOT NULL,
+  `persen` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tipe_user`
+--
+
+CREATE TABLE `tipe_user` (
+  `tipeId` int(11) NOT NULL,
+  `tipeNama` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `transactionId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `waktu` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction_detail`
+--
+
+CREATE TABLE `transaction_detail` (
+  `transactionId` int(11) NOT NULL,
+  `barangId` int(11) NOT NULL,
+  `barangHarga` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userId` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` text NOT NULL,
+  `namaDepan` text NOT NULL,
+  `namaBelakang` text NOT NULL,
+  `email` text NOT NULL,
+  `noHP` text NOT NULL,
+  `saldo` int(11) NOT NULL,
+  `alamat` text NOT NULL,
+  `tipeUser` int(11) NOT NULL,
+  `shoppingCart` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`barangId`),
+  ADD KEY `barang_ibfk_1` (`categoryId`),
+  ADD KEY `brandId` (`brandId`),
+  ADD KEY `promoId` (`promoId`);
+
+--
+-- Indexes for table `brand`
+--
+ALTER TABLE `brand`
+  ADD PRIMARY KEY (`brandId`);
 
 --
 -- Indexes for table `carousel_promo`
@@ -54,14 +180,140 @@ ALTER TABLE `carousel_promo`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`categoryId`);
+
+--
+-- Indexes for table `gambar`
+--
+ALTER TABLE `gambar`
+  ADD PRIMARY KEY (`gambarId`,`barangId`),
+  ADD KEY `barangId` (`barangId`);
+
+--
+-- Indexes for table `promo`
+--
+ALTER TABLE `promo`
+  ADD PRIMARY KEY (`promoId`);
+
+--
+-- Indexes for table `tipe_user`
+--
+ALTER TABLE `tipe_user`
+  ADD PRIMARY KEY (`tipeId`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`transactionId`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `transaction_detail`
+--
+ALTER TABLE `transaction_detail`
+  ADD PRIMARY KEY (`transactionId`,`barangId`),
+  ADD KEY `barangId` (`barangId`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userId`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `tipeUser` (`tipeUser`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `barangId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `brand`
+--
+ALTER TABLE `brand`
+  MODIFY `brandId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `carousel_promo`
 --
 ALTER TABLE `carousel_promo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `categoryId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `gambar`
+--
+ALTER TABLE `gambar`
+  MODIFY `gambarId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `promo`
+--
+ALTER TABLE `promo`
+  MODIFY `promoId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tipe_user`
+--
+ALTER TABLE `tipe_user`
+  MODIFY `tipeId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `transactionId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `barang`
+--
+ALTER TABLE `barang`
+  ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`categoryId`) REFERENCES `category` (`categoryId`),
+  ADD CONSTRAINT `barang_ibfk_2` FOREIGN KEY (`brandId`) REFERENCES `brand` (`brandId`),
+  ADD CONSTRAINT `barang_ibfk_3` FOREIGN KEY (`promoId`) REFERENCES `promo` (`promoId`);
+
+--
+-- Constraints for table `gambar`
+--
+ALTER TABLE `gambar`
+  ADD CONSTRAINT `gambar_ibfk_1` FOREIGN KEY (`barangId`) REFERENCES `barang` (`barangId`);
+
+--
+-- Constraints for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`);
+
+--
+-- Constraints for table `transaction_detail`
+--
+ALTER TABLE `transaction_detail`
+  ADD CONSTRAINT `transaction_detail_ibfk_1` FOREIGN KEY (`transactionId`) REFERENCES `transaction` (`transactionId`),
+  ADD CONSTRAINT `transaction_detail_ibfk_2` FOREIGN KEY (`barangId`) REFERENCES `barang` (`barangId`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
