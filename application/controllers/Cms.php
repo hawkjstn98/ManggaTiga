028f534e6b603410d0fb94aafe23686d6083256a
frapslover63 <39822385+frapslover63@users.noreply.github.com>
2019-04-23 23:08:34 +0700

    Integerating SB Admin 2

---
+++ application/controllers/Cms.php
@@ -0,0 +1,18 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Cms extends CI_Controller{
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function index(){
+        $data['js'] = $this->load->view('include/javascript.php', NULL, TRUE);
+        $data['css'] = $this->load->view('include/css.php', NULL, TRUE);
+        $this->load->view('pages/cms.php', $data);
+    }
+}
+
+?>
--- application/views/include/css.php
+++ application/views/include/css.php
@@ -1,5 +1,9 @@
 <!--CSS-->
 <link rel="stylesheet" type="text/css" href="<?php echo "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css";?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/all.min.css");?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/dataTables.bootstrap4.min.css");?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/sb-admin-2.min.css");?>">
+
 <!--FONT-->
-<link href="https://fonts.googleapis.com/css?family=Fira+Sans" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="<?php echo "http://localhost/assets/css/fontControl.css";?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/fontControl.css");?>">
+<link rel="stylesheet" href="<?php echo"https://fonts.googleapis.com/css?family=Fira+Sans"?>">
