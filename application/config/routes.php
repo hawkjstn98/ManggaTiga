516de7bed55c36015e055a1c30709892bfcbb16d
Mario Alexander <39822647+Xtinguir@users.noreply.github.com>
2019-04-24 23:27:40 +0700

    Redirect Page, Benerin Layout, Nambah Routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,5 +52,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route["Redirect"] = 'RedirectContro    ller';
+$route["Redirect"] = 'RedirectController';
 $route['Redirect/category'] = 'RedirectController/toCategory';
+$route['Redirect/detail'] = 'RedirectController/toDetail';
+$route['Redirect/register'] = 'RedirectController/toRegister';
