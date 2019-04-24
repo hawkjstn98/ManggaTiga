5342aab2bf61d99ab0128d85ae0b78fa5e12bd54
hawkjstn98 <hawk.jstn98@gmail.com>
2019-04-24 21:48:57 +0700

    fix routing, and javascript

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,5 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route["Redirect"] = 'RedirectContro    ller';
+$route["Redirect"] = 'RedirectController';
 $route['Redirect/category'] = 'RedirectController/toCategory';
+$route['cms/Admin'] = 'Cms';
