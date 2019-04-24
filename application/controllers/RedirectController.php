bbc0a8cb1b9844254434767671b86612151b2178
Mario Alexander <39822647+Xtinguir@users.noreply.github.com>
2019-04-24 23:57:03 +0700

    Controller - Shared subPage

--- application/controllers/RedirectController.php
+++ application/controllers/RedirectController.php
@@ -4,31 +4,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 class RedirectController extends CI_Controller {
-
     public function __construct()
     {
         parent::__construct();
+        $this->data['js'] = $this->load->view('include/javascript.php', NULL, TRUE);
+        $this->data['css'] = $this->load->view('include/css.php', NULL, TRUE);
+        $this->data['staticnavbarLoggedin'] = $this->load->view('pages/subPages/staticnavbarLoggedin.php',NULL,TRUE);
+        $this->data['staticnavbarUnloggedin'] = $this->load->view('pages/subPages/staticnavbarUnloggedin.php',NULL,TRUE);
+        $this->data['dynamicnavbar'] = $this->load->view('pages/subPages/dynamicnavbar.php',NULL,TRUE);
     }
-
-
     public function toCategory(){
         $this->load->view("../views/pages/Kategori.php");
     }
     public function toDetail(){
-        getEssential();
-        $this->load->view("../views/pages/Detail.php");        
+        $this->load->view("../views/pages/Detail.php",$this->data);        
     }
     public function toRegister(){
-        $this->load->view("../views/pages/Register.php");
-    }
-
-    //Buat kasi subpage tiap pages
-    public function getEssential(){
-        $data['js'] = $this->load->view('include/javascript.php', NULL, TRUE);
-        $data['css'] = $this->load->view('include/css.php', NULL, TRUE);
-        $data['staticnavbarLoggedin'] = $this->load->view('pages/subPages/staticnavbarLoggedin.php',NULL,TRUE);
-        $data['staticnavbarUnloggedin'] = $this->load->view('pages/subPages/staticnavbarUnloggedin.php',NULL,TRUE);
-        $data['dynamicnavbar'] = $this->load->view('pages/subPages/dynamicnavbar.php',NULL,TRUE);
+        $this->load->view("../views/pages/Register.php",$this->data);
     }
 }
 
