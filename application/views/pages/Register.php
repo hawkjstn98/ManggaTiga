2f31480a9267fd38dbde7ae35734260f82504a0c
Mario Alexander <39822647+Xtinguir@users.noreply.github.com>
2019-04-25 22:12:55 +0700

    Register Layout

--- application/views/pages/Register.php
+++ application/views/pages/Register.php
@@ -6,57 +6,155 @@
       echo $js;
       echo $css;
     ?>
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
-    <style>
-        body {
-            background-color: #1cc88a;
-        }
-    </style>    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <style type="text/css">
+	body {
+		color: #fff;
+		background: #1cc88a;
+		font-family: 'Roboto', sans-serif;
+	}
+	.form-control, .form-control:focus, .input-group-addon {
+		border-color: #e1e1e1;
+	}
+    .form-control, .btn {        
+        border-radius: 3px;
+    }
+	.signup-form {
+		width: 390px;
+		margin: 0 auto;
+		padding: 30px 0;
+	}
+    .signup-form form {
+		color: #999;
+		border-radius: 3px;
+    	margin-bottom: 15px;
+        background: #fff;
+        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+        padding: 30px;
+    }
+	.signup-form h2 {
+		color: #333;
+		font-weight: bold;
+        margin-top: 0;
+    }
+    .signup-form hr {
+        margin: 0 -30px 20px;
+    }
+	.signup-form .form-group {
+		margin-bottom: 20px;
+	}
+	.signup-form label {
+		font-weight: normal;
+		font-size: 13px;
+	}
+	.signup-form .form-control {
+		min-height: 38px;
+		box-shadow: none !important;
+	}	
+	.signup-form .input-group-addon {
+		max-width: 42px;
+		text-align: center;
+	}
+	.signup-form input[type="checkbox"] {
+		margin-top: 2px;
+	}   
+    .signup-form .btn{        
+        font-size: 16px;
+        font-weight: bold;
+		background: #19aa8d;
+		border: none;
+		min-width: 140px;
+    }
+	.signup-form .btn:hover, .signup-form .btn:focus {
+		background: #179b81;
+        outline: none;
+	}
+	.signup-form a {
+		color: #fff;	
+		text-decoration: underline;
+	}
+	.signup-form a:hover {
+		text-decoration: none;
+	}
+	.signup-form form a {
+		color: #19aa8d;
+		text-decoration: none;
+	}	
+	.signup-form form a:hover {
+		text-decoration: underline;
+	}
+	.signup-form .fa {
+		font-size: 21px;
+	}
+	.signup-form .fa-paper-plane {
+		font-size: 18px;
+	}
+	.signup-form .fa-check {
+		color: #fff;
+		left: 17px;
+		top: 18px;
+		font-size: 7px;
+		position: absolute;
+	}
+</style>
 </head>
-
 <body>
-    <div class="container card" style="width:600px;">
-        <form>
-            <div class="form-header" align="center">
-                <h1>manggatiga.com</h1>
-                <br>
-                <h3>Register</h3>
-            </div>
-            <br>
-            <div class="row">
-                <div class="col-6 form-group">
-                    <label for="firstnRegist">Nama Depan</label>
-                    <input type="text" class="form-control" id="firstnRegist">
-                </div>
-                <div class="col-6 form-group">
-                    <label for="lastnRegist">Nama Belakang</label>
-                    <input type="text" class="form-control" id="lastnRegist">
+<div class="signup-form">
+    <form action="">
+        <div class="form-header" align="center">
+            <h2>Register</h2>
+            <p>manggatiga.com</p>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-6 form-group">
+                <div class="input-group">
+                    <input type="text" class="form-control" name="firstname" placeholder="First Name" required="required">
                 </div>
             </div>
-            <div class="form-group">
-                <label for="emailRegist">Email</label>
-                <input style="width:350px;" type="email" class="form-control" id="emailRegist">
+            <div class="col-6 form-group">
+                <div class="input-group">
+                    <input type="text" class="form-control" name="lastname" placeholder="Last Name" required="required">
+                </div>
             </div>
-            <div class="form-group">
-                <label for="lastnRegist">Username</label>
-                <input style="width:250px;" type="text" class="form-control" id="lastnRegist">
-            </div>
-            <div class="form-group">
-                <label for="lastnRegist">Password</label>
-                <input style="width:250px;" type="password" class="form-control" id="lastnRegist">
-            </div>
-            <div class="form-group">
-                <label for="phonenoRegist">Nomor HP</label>
-                <input style="width:250px;" type="text" class="form-control" id="phonenoRegist">
-            </div>
-            <div class="form-group">
-                <label for="alamatRegist">Alamat</label>
-                <input style="width:450px;" type="text" class="form-control" id="alamatRegist">
-            </div>
-            <div class="form-footer row">
-                <div class="col-4"></div>
-                <button type="submit" class="col-4 btn btn-success">Register</button>    
-            </div>
-        </form>
-    </div>
+        </div>
+        <div class="form-group">
+			<div class="input-group">
+				<span class="input-group-addon"><i class="fa fa-user"></i></span>
+				<input type="text" class="form-control" name="username" placeholder="Username" required="required">
+			</div>
+        </div>
+        <div class="form-group">
+			<div class="input-group">
+				<span class="input-group-addon"><i class="fa fa-paper-plane"></i></span>
+				<input type="email" class="form-control" name="email" placeholder="Email" required="required">
+			</div>
+        </div>
+		<div class="form-group">
+			<div class="input-group">
+				<span class="input-group-addon"><i class="fa fa-lock"></i></span>
+				<input type="text" class="form-control" name="password" placeholder="Password" required="required">
+			</div>
+        </div>
+		<div class="form-group">
+			<div class="input-group">
+				<span class="input-group-addon"><i class="fa fa-home"></i></span>
+				<input type="text" class="form-control" name="address" placeholder="Address" required="required">
+			</div>
+        </div>
+        <div class="form-group">
+			<div class="input-group">
+				<span class="input-group-addon"><i class="fa fa-mobile"></i></span>
+				<input type="text" class="form-control" name="phonenumber" placeholder="Phone Number" required="required">
+			</div>
+        </div>
+        <div class="form-group">
+			<label class="checkbox-inline"><input type="checkbox" required="required"> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
+		</div>
+		<div class="form-group">
+            <button type="submit" class="btn btn-primary btn-lg">Sign Up</button>
+        </div>
+    </form>
+</div>
 </body>
+</html>
