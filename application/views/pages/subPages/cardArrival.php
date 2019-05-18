dfc26d74d29679821d3ad17ad73784861f6ba8f8
hawkjstn98 <hawk.jstn98@gmail.com>
2019-05-18 17:00:44 +0700

    fix

--- application/views/pages/subPages/cardArrival.php
+++ application/views/pages/subPages/cardArrival.php
@@ -1,17 +1,7 @@
 <!-- Card Item dipakai di page awal dan page kategori, masih sangat polos boleh dihias :") -->
 <div class="container">
     <div id="cardgridarrive" class="row">
-        <!--        <div class="card" style="width: 18rem; margin-left:50px;">-->
-        <!--            <a href="--><?php //echo base_url('index.php/Redirect/detail');?><!--">-->
-        <!--                <img class="card-img-top" alt="Card image cap" src="http://localhost/assets/logo/Ourlogo.png">-->
-        <!--                <div class="card-body">-->
-        <!--                    <h5 class="card-title" align="center" id="cardNamaBarang">MWB</h5>-->
-        <!--                </div>-->
-        <!--            </a>-->
-        <!--            <div class="card-bottom" align="center" style="margin-bottom:10px">-->
-        <!--                <p class="card-text" id="cardHarga">GRATIS</p>-->
-        <!--            </div>-->
-        <!--        </div>-->
+
     </div>
 </div>
 <script>
