4c15ed885a40036759fa941e4af805474db936ae
Mario Alexander <39822647+Xtinguir@users.noreply.github.com>
2019-05-18 16:19:41 +0700

    Search Page

--- application/controllers/RedirectController.php
+++ application/controllers/RedirectController.php
@@ -27,6 +27,10 @@ class RedirectController extends CI_Controller {
         $this->load->view("../views/pages/Detail.php",$this->data);        
     }
 
+    public function toSearch(){
+        $this->load->view("../views/pages/Search.php",$this->data);
+    }
+
     public function toShoppingChart(){
         $this->data['emptycart'] = $this->load->view('pages/subPages/emptyCart.php',NULL,TRUE);
         $this->data['filledcart'] = $this->load->view('pages/subPages/filledCart.php',NULL,TRUE);
