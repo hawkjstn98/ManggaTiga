6ae662a0b384dab00f51a7844e26ae2ec4f19007
frapslover63 <39822385+frapslover63@users.noreply.github.com>
2019-05-18 15:59:01 +0700

    Currency Formatting

--- application/views/include/javascript.php
+++ application/views/include/javascript.php
@@ -8,5 +8,3 @@
 <script src="<?php echo base_url('assets/js/jquery.dataTables.min.js'); ?>"></script>
 <script src="<?php echo base_url('assets/js/jquery.easing.min.js'); ?>"></script>
 <script src="<?php echo base_url('assets/js/sb-admin-2.min.js'); ?>"></script>
-
-        
