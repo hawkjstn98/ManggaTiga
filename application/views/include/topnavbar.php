8cd0eea43b613b9ee6ee0e5fe178b9304ff724dd
hawkjstn98 <hawk.jstn98@gmail.com>
2019-05-19 18:21:32 +0700

    fix for cms topnavbar

--- application/views/include/topnavbar.php
+++ application/views/include/topnavbar.php
@@ -32,8 +32,8 @@
   <!-- Nav Item - User Information -->
   <li class="nav-item dropdown no-arrow">
     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      <span class="mr-2 d-none d-lg-inline text-gray-600 small">Utin</span>
-      <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
+      <span class="mr-2 d-none d-lg-inline text-gray-600 small"> <?php if($this->session->userdata('user')=='ADMINISTRATORMWB') echo $this->session->userdata('user');?></span>
+      <img class="img-profile rounded-circle" src="<?php echo base_url('assets/logo/Ourlogo.png')?>">
     </a>
     <!-- Dropdown - User Information -->
     <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
